29c8310d52ae89a7a7aeaf172bccf4909dfd8253
Phiwo Njemla <phiwo041590@gmail.com>
2023-08-14 20:23:05 +0000

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,7 +9,7 @@ void print_python_list_info(PyObject *p)
 
 {
 	int s, allo, i;
-	PyObject *ob;
+	PyObject *obj;
 
 	s = Py_SIZE(p);
 	allo = ((PyListObject *)p)->allocated;
@@ -21,7 +21,7 @@ void print_python_list_info(PyObject *p)
 	{
 		printf("Element %d: ", i);
 
-		ob = PyList_GetItem(p, i);
+		obj = PyList_GetItem(p, i);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }

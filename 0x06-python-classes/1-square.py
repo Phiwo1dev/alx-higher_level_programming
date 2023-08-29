c09440dfac18ebd86002795d112942e3c015c0c3
Phiwo Njemla <phiwo041590@gmail.com>
2023-08-29 06:16:20 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A class Square that defines a square"""
+
+
+class Square:
+    """represents a square."""
+
+    def __init__(self, size):
+        """Initialize New Square.
+
+        Args:
+            size (int): Size of the new square.
+        """
+        self.__size = size

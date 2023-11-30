c031326231ee1e41d34d091fa0c92f679b6717b7
Phiwo Njemla <phiwo041590@gmail.com>
2023-11-30 14:45:59 -0800

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# This script sends a POST request and two variables.
+curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
